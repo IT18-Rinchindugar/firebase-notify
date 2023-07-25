f59d9cacebfd9b01e84f4bd6516ec81d408f1549
Rinchindugar <rinchindugar@tomyo.mn>
2023-07-26 02:35:20 +0800

    feat: add the body validation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
+import { body, validationResult } from 'express-validator';
 import notificationService from './service/notificationService.js';
 
 dotenv.config();
@@ -15,17 +16,28 @@ app.get('/', (req, res) => {
   res.send('Hello Server..');
 });
 
-app.post('/notification/webhook', (req, res) => {
-  const { title, text, appDeviceId } = req.body;
-  const msg = {
-    notification: { title, body: text },
-    data: {},
-  };
-  notificationService.sendMultiCastPushNotification([appDeviceId], msg)
-  res.status(200).json({
-    success: true
+app.post('/notification/webhook',
+  body('appDeviceId').notEmpty(),
+  body('title').notEmpty(),
+  body('text').notEmpty(),
+  (req, res) => {
+    const result = validationResult(req);
+
+    if (result.isEmpty()) {
+      const { title, text, appDeviceId, link } = req.body;
+      const msg = {
+        notification: { title, body: text },
+        data: {
+          link: link || 'cardoctor://'
+        },
+      };
+      notificationService.sendMultiCastPushNotification([appDeviceId], msg)
+      return res.status(200).json({
+        success: true
+      });
+    }
+    res.status(400).json({ errors: result.array() });
   });
-});
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`App listening on port ${process.env.PORT || 5000}`);

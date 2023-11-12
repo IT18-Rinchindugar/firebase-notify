d07af124d5679069f894897c29e94784d5836278
Rinchindugar <rinchindugar@tomyo.mn>
2023-11-12 11:10:57 +0800

    android update

--- src/utils.js
+++ src/utils.js
@@ -3,8 +3,8 @@ export const binaryVersion = '1.0.18';
 export const binaryRelease = true;
 
 // app - iin odoo baigaa version - iig zaaj ogno
-export const ANDROID_APP_VERSION = '1.0.1';
+export const ANDROID_APP_VERSION = '1.0.17';
 
 // shineer tataj avah version - iig zaaj ogno
-export const ANDROID_BINARY_VERSION = '1.0.17';
+export const ANDROID_BINARY_VERSION = '1.0.18';
 export const ANDROID_BINARY_RELEASE = true;
